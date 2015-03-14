bac357728c100748d95d436e7a6938ee8e6b2b82
Maxim Vasilchuk <mvasilchuk@gmail.com>
2015-03-14 02:00:22 +0200

    Some improvements in build script

--- build_package.sh
+++ build_package.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
-CURRENT_DIR=$(pwd)
-TMP_DIR="/tmp/yasem-$(uname -p)"
-CURR_DATETIME="$(date +"%F_%H-%M-%S")"
-OUT_DIR="${CURR_DATETIME}-$(git rev-parse --short HEAD)"
+TMP_DIR="/tmp"
+YASEM_OUT_DIR="yasem-$(uname -m)-$(date +"%F_%H-%M-%S")-$(git rev-parse --short HEAD)"
+FULL_OUTPUT_DIR="${TMP_DIR}/${YASEM_OUT_DIR}"
 CPU_COUNT=$(nproc)
 
-echo $OUT_DIR
-
-/opt/Qt/5.4/gcc_64/bin/qmake -recursive YASEM_OUTPUT_DIR="${TMP_DIR}/${OUT_DIR}"
+/opt/Qt/5.4/gcc_64/bin/qmake -recursive YASEM_OUTPUT_DIR=${FULL_OUTPUT_DIR}
 make -j${CPU_COUNT}
-echo $TMP_DIR
-cd $TMP_DIR
-tar -zcvf $1/yasem-${CURR_DATETIME}.tar.gz $OUT_DIR 
+cd ${TMP_DIR}
+tar -zcvf $1/${YASEM_OUT_DIR}.tar.gz ${YASEM_OUT_DIR}
 cd -
+rm "${FULL_OUTPUT_DIR}" -r
