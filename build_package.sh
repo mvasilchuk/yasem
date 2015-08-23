e82457cdab260cb3a3e3070b1164905ae6896a2d
Maxim Vasilchuk <mvasilchuk@gmail.com>
2015-08-24 01:35:48 +0300

    Update build_package.sh

--- build_package.sh
+++ build_package.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+ARCH=64
 TMP_DIR="/tmp"
 YASEM_OUT_DIR="yasem-$(uname -m)-$(date +"%F_%H-%M-%S")-$(git rev-parse --short HEAD)"
 FULL_OUTPUT_DIR="${TMP_DIR}/${YASEM_OUT_DIR}"
 CPU_COUNT=$(nproc)
 
-/opt/Qt/5.4/gcc_64/bin/qmake -recursive YASEM_OUTPUT_DIR=${FULL_OUTPUT_DIR}
+$QTDIR/gcc_${ARCH}/bin/qmake -recursive YASEM_OUTPUT_DIR=${FULL_OUTPUT_DIR}
 make -j${CPU_COUNT}
 cd ${TMP_DIR}
 tar -zcvf $1/${YASEM_OUT_DIR}.tar.gz ${YASEM_OUT_DIR}
